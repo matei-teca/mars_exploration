5478186ed237204bf19b3a8f6504d142ba52f66e
Valentin1289 <valibaiacof1289@gmail.com>
2023-05-24 16:49:31 +0300

    Changes by eMarean

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
-
+        Random random = new Random();
         LocalDateTime now = LocalDateTime.now();
         String filePath = "src/main/resources/exploration" + "-" +
             now.toString().replaceAll(":", "-").replaceAll("\\.","-")
@@ -20,7 +20,7 @@ public class Application {
         ExplorationSimulator simulator = new ExplorationSimulator();
         SimulationInput input = new SimulationInput(
                 "src/main/resources/exploration-2.map",
-                new Coordinate(29, 26), 235, filePath);
+                new Coordinate(random.nextInt(2,30), random.nextInt(2, 30)), 400, filePath);
         simulator.simulate(input);
 
     }
